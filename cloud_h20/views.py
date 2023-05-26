aa7453683544509d46b23a994984018e95d9a7b1
Angelica Basista <angelicabasista1@gmail.com>
2023-05-27 01:20:11 +0800

    Display dummy data for sensors

--- cloud_h20/views.py
+++ cloud_h20/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    sensors = [{'name': 'TDS sensor', 'state': 'potable'},
+               {'name': 'pH sensor', 'state': 'nonpotable'}]
+    return render(request, "index.html", {'sensors': sensors})
