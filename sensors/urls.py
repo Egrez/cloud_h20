60fd0e3d60c81fca388fa6429a67772b61726294
serge <sergerivera@upcapes.org>
2023-05-27 21:53:09 +0800

    added url for sign in and warn

---
+++ sensors/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('sensor', views.sensor, name='sensor'),
+    path('warn', views.send_warning_signal, name='warn'),
+    path('signin', views.signin, name='signin'),
+]
