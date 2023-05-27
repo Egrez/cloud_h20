9533e81519da1145de1d5adb4868487b7ad07748
Bourbon-Rye <69958552+Bourbon-Rye@users.noreply.github.com>
2023-05-27 14:26:05 +0800

    Update views.py

--- cloud_h20/views.py
+++ cloud_h20/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Sensors, ServerData
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def readings(request): # POST Request for Sensor Readings
+    device_id = request.POST['device_id']
+    location = request.POST['location']
+    data = request.POST['data']
+
+    #Timestamp should be automatically assigned when saved
+    new_obj = Sensors(device_id=device_id, location=location, data=data)
+    new_obj.save()
+
+    # Render the HTML template with the data in the context variable
+    # return render(request, 'index.html', context=context)
