058523058b1e8e8f34035b98f2a2610efea0f2d2
serge <sergerivera@upcapes.org>
2023-05-27 21:55:07 +0800

    added sensors, sensor readings and server data into the admin interface

--- sensors/admin.py
+++ sensors/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Sensors, ServerData
+from .models import Sensor, ServerData, SensorReading
 
 # Register your models here.
-admin.site.register(Sensors)
+admin.site.register(Sensor)
+admin.site.register(SensorReading)
 admin.site.register(ServerData)
