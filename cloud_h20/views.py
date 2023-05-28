13d8985428639bbb6f9350882c5c2b087334c66f
Angelica Basista <angelicabasista1@gmail.com>
2023-05-28 13:16:56 +0800

    Add buttons -- buttons don't do anything yet

--- cloud_h20/views.py
+++ cloud_h20/views.py
@@ -6,4 +6,7 @@ def home(request):
     sensors = Sensor.objects.all()
     sensor_readings = SensorReading.objects.all()
     server_data = ServerData.objects.all()
-    return render(request, "index.html", {'sensors': sensors, 'sensor_readings': sensor_readings, 'server_data': server_data})
+    locations = Sensor.objects.values_list('location', flat=True).distinct()
+
+    return render(request, "index.html", { 'sensors': sensors, 'sensor_readings': sensor_readings, 
+                                          'server_data': server_data, 'locations': locations })
