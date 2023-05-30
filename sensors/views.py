4c4a7c67e697e376216d9eac40d25bff24e080d9
serge <sergerivera@upcapes.org>
2023-05-30 23:09:30 +0800

    added port and ip address to sensor view and updated warning signal to include sensor id

--- sensors/views.py
+++ sensors/views.py
@@ -6,15 +6,19 @@ import requests
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
-def home(request):
-	return render(request, "index.html")
-
 @csrf_exempt
 def signin(request):
 	username = request.POST["username"]
 	password = request.POST["password"]
+	port = request.POST["port"]
 	user = authenticate(request, username=username, password=password)
+
+	sensor = user.sensor
+	ip_addr, _ = request.environ["wsgi.input"].stream.raw._sock.getpeername()
+	sensor.ip_addr = ip_addr
+
+	sensor.port = port
+	sensor.save()
 
 	if user is not None:
 		login(request, user)
@@ -27,22 +31,33 @@ def sensor(request): # POST Request for Sensor Readings
 		user = request.user
 		device_id = user.sensor
 		
-		data = request.POST['data']
+		tds = request.POST['tds']
+		pH = request.POST['pH']
+		cond = request.POST['cond']
 
 		#Timestamp should be automatically assigned when saved
-		new_obj = SensorReading(device_id=device_id, data=data)
+		new_obj = SensorReading(device_id=device_id, tds=tds, pH=pH, cond=cond)
 		new_obj.save()
 
 	# Render the HTML template with the data in the context variable
 	return render(request, "index.html")
 
-def send_warning_signal(request): # POST Request to turn on LEDs
-	post_data = {'name': 'Gladys'}
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+def send_warning_signal(request, id): # POST Request to turn on LEDs
+	user = User.objects.all()[0]
+
+	sensor = user.sensor
+
+	ip_addr = sensor.ip_addr
+	port = sensor.port
+
+	post_data = {'valve': False, 'LED' : True}
 
 	# replace this with IP address and port number of client sensors
-	response = requests.post('http://example.com', data=post_data)
+	response = requests.post(f'http://{ip_addr}:{port}', data=post_data)
 	content = response.content
 
-	print(content)
 	# Render the HTML template with the data in the context variable
 	return render(request, "index.html")
