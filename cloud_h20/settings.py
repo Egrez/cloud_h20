bd28a0974a3b36920732c94def100f6f9b57cefb
serge <sergerivera@upcapes.org>
2023-06-01 15:14:28 +0800

    added custom domain

--- cloud_h20/settings.py
+++ cloud_h20/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-*g=5ph*saj&p^#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['deployed-cfscheduler-production.up.railway.app', 'https://deployed-cfscheduler-production.up.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['cloud-h20.up.railway.app', 'https://cloud-h20.up.railway.app', '127.0.0.1', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://deployed-cfscheduler-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://cloud-h20.up.railway.app']
 
 # Application definition
 
