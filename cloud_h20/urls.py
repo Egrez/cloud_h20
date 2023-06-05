cd3772b15dd9f91519d05d4fc8813cf0cd29e866
serge <sergerivera@upcapes.org>
2023-06-06 01:54:16 +0800

    removed index url

--- cloud_h20/urls.py
+++ cloud_h20/urls.py
@@ -23,7 +23,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    #path('', views.index, name = 'index'),
     path('', include('sensors.urls')),
 ]
 
